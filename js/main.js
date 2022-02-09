f444506cb9ce529bedcb25326b75daabb305446f
Santiago Velasco <santiagovelascomosquera@hotmail.com>
2022-02-09 15:49:21 -0500

    footer terminado

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var entorno = document.getElementById("harriba")
+addEventListener("scroll", prueba)
+
+function prueba()
+{
+    entorno.classList.remove('harriba')
+    entorno.classList.add('habajo')
+}
